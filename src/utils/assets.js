ede5b209e189f126b8bc862aacfcce5380ee7cd9
pungki wibowo <pungkiwibowo29@gmail.com>
2026-03-27 18:52:31 +0700

    update: perbaiki koneksi API frontend ke backend

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -1,7 +1,6 @@
 export const resolveImage = (imgName) => {
     if (!imgName) return '';
     if (imgName.startsWith('http')) return imgName;
-    
     // Dynamically resolve the image path using Vite's URL feature.
     // This eliminates the need for any hardcoded imports.
     return new URL(`../assets/fleets/${imgName}`, import.meta.url).href;
